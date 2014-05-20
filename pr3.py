d36c79f6de1dcf99acfda67195e0650e188cef16
Prakhar Bhandari <prakharb@seas.upenn.edu>
2014-05-20 13:08:35 -0400

    finished pr3

--- pr3.py
+++ pr3.py
@@ -1,12 +1,8 @@
 import mathtools
 
-print 600851475143
-
 def largest_prime_factor(n):
-    start = n
-    while n % 2 != 0 and not mathtools.is_prime(n):
-        n -= 1
-    return n
+    factorization = mathtools.prime_factorization(n)
+    return max(factorization.keys())
 
 
 print largest_prime_factor(600851475143)
