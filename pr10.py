73105d26ceec92c67175c72d62d3504f4fd28644
Prakhar Bhandari <prakharb@seas.upenn.edu>
2014-05-20 23:07:24 -0400

    fixed pr10

--- pr10.py
+++ pr10.py
@@ -1,32 +1,12 @@
-# import mathtools
+import mathtools
 
-# def sum_of_primes_under(n):
-#     start = 3
-#     total = 2
-#     while start < n:
-#         if mathtools.is_prime(start):
-#             total += start
-#         start += 2
-#         if start < n and mathtools.is_prime(start):
-#             total += start
-#         start += 4
-#     return total
+def sum_of_primes_under(n):
+    start = 2
+    total = 0
+    while start < n:
+        total += start
+        start = mathtools.get_next_prime(start)
+    return total
 
-# print sum_of_primes_under(2000000)
+print sum_of_primes_under(2000000)
 
-import math
-prime_list = []
-numb = 3
-
-def prime_check(n):
-    for i in xrange(2, int(math.sqrt(n)) + 1):
-         if n % i == 0:
-             return
-    else:
-        prime_list.append(n)
-
-while numb < 2000000:
-    prime_check(numb)
-    numb += 2
-
-print sum(prime_list)
