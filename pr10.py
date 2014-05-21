7a582c43b1a85ddc8a45484c4a5d00b80a379d87
Prakhar Bhandari <prakharb@seas.upenn.edu>
2014-05-20 23:09:15 -0400

    pr10 minor style changes

--- pr10.py
+++ pr10.py
@@ -1,11 +1,11 @@
-import mathtools
+from mathtools import get_next_prime
 
 def sum_of_primes_under(n):
     start = 2
     total = 0
     while start < n:
         total += start
-        start = mathtools.get_next_prime(start)
+        start = get_next_prime(start)
     return total
 
 print sum_of_primes_under(2000000)
