2c1f25213450db7eb4b7b6e62d7b988f4a301778
Prakhar Bhandari <prakharb@seas.upenn.edu>
2014-05-23 10:58:01 -0400

    finished pr13

--- pr13.py
+++ pr13.py
@@ -1,2 +1,8 @@
-def first_n_digits_of_sum(file):
-    
+def first_n_digits_of_sum(file, n):
+    fin = open(file)
+    total = 0
+    for i in fin:
+        total += int(i.strip())
+    return str(total)[:n]
+
+print first_n_digits_of_sum('pr13.txt', 10)
