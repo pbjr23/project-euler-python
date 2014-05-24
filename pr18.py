05e028dd768d5d1a6214c371c12080229553af23
Prakhar Bhandari <prakharb@seas.upenn.edu>
2014-05-23 23:37:08 -0400

    finished pr18, pr67

--- pr18.py
+++ pr18.py
@@ -1,7 +1,15 @@
-fin = open('pr18.txt')
-all = []
-for i in fin:
-	all.append(i.strip().split(' '))
+def maximum_path(file):
+    data = []
+    fin = open(file)
+    for line in fin:
+        data.append([int(i) for i in line.strip().split()])
+    for line in range(1, len(data)):
+        line_length = len(data[line])
+        data[line][0] += data[line - 1][0]
+        for element in range(1, line_length - 1):
+            greater = max(data[line - 1][element], data[line - 1][element - 1])
+            data[line][element] += greater
+        data[line][line_length - 1] += data[line - 1][line_length - 2]
+    return max(data[-1])
 
-for row in range(len(all)):
-	print all[row]
+# print maximum_path('pr18.txt')
---
+++ pr67.py
@@ -0,0 +1,3 @@
+from pr18 import maximum_path
+
+print maximum_path('pr67.txt')
